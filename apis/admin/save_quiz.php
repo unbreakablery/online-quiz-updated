38181cf5328c7e1b0d85168f3e50717aef48b7f3
unbreakablery <unbreakablery@gmail.com>
2021-09-03 16:42:28 +0900

    added apis

---
+++ apis/admin/save_quiz.php
@@ -0,0 +1,77 @@
+<?php
+    require '../../inc/connect_db.php';
+    
+    $quiz_id    = $_POST['quiz-id'];
+    $old_quiz_code  = $_POST['old-quiz-code'];
+    $quiz_code  = $_POST['quiz-code'];
+    $quiz_type  = $_POST['quiz-type'];
+    $quiz_kind  = $_POST['quiz-kind'];
+    $page  = $_POST['page'];
+    
+    if ($quiz_type == "untimed") {
+        $limit_time = 0;
+    } else {
+        $limit_time = $_POST['limit-time'];
+    }
+
+    $data = array(
+                    'quiz_id'       => $quiz_id,
+                    'quiz_code'     => $quiz_code,
+                    'quiz_type'     => $quiz_type,
+                    'limit_time'    => $limit_time,
+                    'quiz_kind'     => $quiz_kind,
+                    'page'          => $page
+                );
+
+    if ($quiz_id == 0) {
+        //in case save new quiz
+        if (existQuiz($quiz_id, $quiz_code, $quiz_type)) {
+            echo json_encode(
+                array(
+                    "status"    => false,
+                    "msg"       => "Quiz with this code exists already!"
+                )
+            );
+            exit;
+        } else {
+            saveQuiz($data);
+            echo json_encode(
+                array(
+                    "status"    => true,
+                    "msg"       => "Quiz was saved successfully!"
+                )
+            );
+            exit;
+        }
+    } else {
+        //in case update quiz
+        if ($old_quiz_code == $quiz_code) {
+            updateQuiz($data);
+            echo json_encode(
+                array(
+                    "status"    => true,
+                    "msg"       => "Quiz was updated successfully!"
+                )
+            );
+            exit;
+        }
+        if (existQuiz($quiz_id, $quiz_code, $quiz_type)) {
+            echo json_encode(
+                array(
+                    "status"    => false,
+                    "msg"       => "Quiz with this code-[$quiz_code] exists already!"
+                )
+            );
+            exit;
+        } else {
+            updateQuiz($data);
+            echo json_encode(
+                array(
+                    "status"    => true,
+                    "msg"       => "Quiz was updated successfully!"
+                )
+            );
+            exit;
+        }
+    }
+?>
